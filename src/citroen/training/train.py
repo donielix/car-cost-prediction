13af376ff167c1992726013b167720ab93b66532
Daniel Diego Horcajuelo <dadiego91@gmail.com>
2023-04-01 12:01:53 +0200

    updated train script and gitignore

--- src/citroen/training/train.py
+++ src/citroen/training/train.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Training script",
+        description="Fits the model according to the data passed",
+        epilog="End of help",
+    )
+    parser.add_argument(
+        "--training-data", type=str, required=True, help="Source path for training data"
+    )
+    parser.add_argument(
+        "--hyperparams-file",
+        type=str,
+        required=True,
+        help="Source path for hyperparameters file",
+    )
+    parser.add_argument(
+        "--artifacts-data",
+        type=str,
+        required=True,
+        help="Path where to store output artifacts",
+    )
