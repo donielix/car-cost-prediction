39539421e575741e67e8db2d0f8535e03af11a10
Daniel Diego Horcajuelo <dadiego91@gmail.com>
2023-05-18 23:53:19 +0200

    exponential added

--- src/citroen/models/exponential.py
+++ src/citroen/models/exponential.py
@@ -1,18 +1,34 @@
+from typing import List
 from scipy.optimize import curve_fit
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+import numpy as np
 
 
 class ExponentialModel(BaseEstimator, RegressorMixin):
+    def __init__(self, initial_params: List[float]) -> None:
+        self.initial_params = initial_params
+
     @staticmethod
-    def _model_func(x, w0, w1, w2):
-        pass
+    def _model_func(x, w0, w1, w2, w3):
+        distance = x[:, 0]
+        mileage = x[:, 1]
+        fuel_price = x[:, 2]
+
+        consumption = w0 + w1 * np.exp(-w2 * distance + w3)
+        price = consumption * distance * fuel_price
+        return price
+
     def fit(self, X, y):
         # Check that X and y have correct shape
         X, y = check_X_y(X, y)
         # Store the classes seen during fit
         self.X_ = X
         self.y_ = y
+        self.best_params_, pcov = curve_fit(
+            self._model_func, xdata=X, ydata=y, p0=self.initial_params
+        )
+        self.estimation_err_ = np.sqrt(np.diag(pcov))
         # Return the classifier
         return self
 
@@ -21,3 +37,4 @@ class ExponentialModel(BaseEstimator, RegressorMixin):
         check_is_fitted(self)
         # Input validation
         X = check_array(X)
+        return self._model_func(X, *self.best_params_)
